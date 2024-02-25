cb15d12f2426c5d59c9388ea8d1236d556aa0c0d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-25 10:07:42 +0100

    Update ModMixinConfigPlugin.java

--- 1.20/Forge/src/main/java/fuzs/enchantmentcontrol/mixin/ModMixinConfigPlugin.java
+++ 1.20/Forge/src/main/java/fuzs/enchantmentcontrol/mixin/ModMixinConfigPlugin.java
@@ -26,13 +26,14 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class ModMixinConfigPlugin implements IMixinConfigPlugin {
-    private static final Map<String, String> DYNAMIC_MIXINS;
+    private static final Map<String, String> DYNAMIC_MIXINS = Maps.newLinkedHashMap();
     private static boolean applied;
 
+    private final Map<String, byte[]> additionalUrls = Maps.newHashMap();
+    private final MapBackedURLStreamHandler urlStreamHandler = new MapBackedURLStreamHandler();
+
     static {
-        Map<String, String> map = Maps.newLinkedHashMap();
-        map.put("DynamicEnchantmentMixin", ".enchantmentcontrolcache");
-        DYNAMIC_MIXINS = Collections.unmodifiableMap(map);
+        DYNAMIC_MIXINS.put("DynamicEnchantmentMixin", ".enchantmentcontrolcache");
     }
 
     @Override
@@ -68,27 +69,14 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
 
             InputStream inputStream = this.getClass().getResourceAsStream(mixinClassName + ".class");
             Objects.requireNonNull(inputStream, "input stream is null");
+
             try {
 
-                ClassReader classReader = new ClassReader(inputStream);
-                inputStream.close();
-
-                ClassNode classNode = new ClassNode();
-                classReader.accept(classNode, 0);
-
-                classNode.name = mixinClassName;
-                classNode.invisibleAnnotations.removeIf(annotationNode -> annotationNode.desc.equals("Lorg/spongepowered/asm/mixin/Mixin;"));
-                AnnotationVisitor mixinAnnotation = classNode.visitAnnotation("Lorg/spongepowered/asm/mixin/Mixin;", false);
-                AnnotationVisitor targetAnnotation = mixinAnnotation.visitArray("targets");
-                for (String target : targets) {
-                    targetAnnotation.visit(null, target);
-                }
-                targetAnnotation.visitEnd();
-                mixinAnnotation.visitEnd();
-
-                ClassWriter classWriter = new ClassWriter(0);
-                classNode.accept(classWriter);
-                byte[] byteArray = classWriter.toByteArray();
+                String _mixinClassName = mixinClassName;
+                String[] _targets = targets;
+                byte[] byteArray = readAndTransformClass(inputStream, classNode -> {
+                    modifyClassNode(_mixinClassName, _targets, classNode);
+                });
 
                 addUrl(this.getClass().getClassLoader(), '/' + mixinPackage + "/$/" + mixinClassName + ".class", byteArray);
 
@@ -96,6 +84,34 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private static byte[] readAndTransformClass(InputStream inputStream, Consumer<ClassNode> transformer) throws IOException {
+        ClassReader classReader = new ClassReader(inputStream);
+        inputStream.close();
+        ClassNode classNode = new ClassNode();
+        classReader.accept(classNode, 0);
+        transformer.accept(classNode);
+        ClassWriter classWriter = new ClassWriter(0);
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+
+    private static void modifyClassNode(String mixinClassName, String[] targets, ClassNode classNode) {
+        classNode.name = mixinClassName;
+        classNode.invisibleAnnotations.removeIf(annotationNode -> annotationNode.desc.equals("Lorg/spongepowered/asm/mixin/Mixin;"));
+        AnnotationVisitor mixinAnnotation = classNode.visitAnnotation("Lorg/spongepowered/asm/mixin/Mixin;", false);
+        AnnotationVisitor targetAnnotation = mixinAnnotation.visitArray("targets");
+        for (String target : targets) {
+            targetAnnotation.visit(null, target);
+        }
+        targetAnnotation.visitEnd();
+        mixinAnnotation.visitEnd();
+    }
+
+    private static String moveToSubpackage(String className, String subPackage) {
+        char separator = className.contains(".") ? '.' : '/';
+        return className.substring(0, className.lastIndexOf(separator) + 1) + subPackage + separator;
     }
 
     @Override
@@ -129,23 +145,28 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
     }
 
     private static void addUrl(ClassLoader classLoader, String urlPath, byte[] byteArray) {
-        Class<?> clazz = ModuleClassLoader.class;
         if (!(classLoader instanceof ModuleClassLoader)) throw new IllegalStateException("Class loader not supported " + classLoader);
         try {
             Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
             theUnsafe.setAccessible(true);
             Unsafe unsafe = (Unsafe) theUnsafe.get(null);
-            Field parentLoadersField = clazz.getDeclaredField("parentLoaders");
+            Field parentLoadersField = ModuleClassLoader.class.getDeclaredField("parentLoaders");
             long parentLoadersOffset = unsafe.objectFieldOffset(parentLoadersField);
             Map<String, ClassLoader> parentLoaders = (Map<String, ClassLoader>) unsafe.getObject(classLoader, parentLoadersOffset);
             String urlDomain = urlPath.replace('/', '.').substring(1, urlPath.lastIndexOf('/'));
             ClassLoader parentLoader = parentLoaders.remove(urlDomain);
             if (parentLoader != null) throw new RuntimeException("Domain for URL '" + urlPath + "' already registered");
-            URL url = new URL("", null, -1, "/", new SingletonURLStreamHandler(urlPath, byteArray));
+            URL url = new URL("", null, -1, "/", new MapBackedURLStreamHandler(urlPath, byteArray));
             parentLoaders.put(urlDomain, new URLClassLoader(new URL[]{url}, EmptyClassLoader.INSTANCE));
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String getUrlPackage(String urlPath) {
+        int startIndex = urlPath.startsWith("/") ? 1 : 0;
+        int endIndex = urlPath.lastIndexOf('/');
+        return urlPath.replace('/', '.').substring(startIndex, endIndex);
     }
 
     private static class EmptyClassLoader extends ClassLoader {
@@ -182,18 +203,11 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
         }
     }
 
-    private static class SingletonURLStreamHandler extends URLStreamHandler {
-        private final String urlPath;
-        private final byte[] byteArray;
-
-        public SingletonURLStreamHandler(String urlPath, byte[] byteArray) {
-            this.urlPath = urlPath;
-            this.byteArray = byteArray;
-        }
+    private class MapBackedURLStreamHandler extends URLStreamHandler {
 
         @Override
         protected URLConnection openConnection(URL url) throws IOException {
-            if (!this.urlPath.equals(url.getPath())) return null;
+            if (!ModMixinConfigPlugin.this.additionalUrls.containsKey(url.getPath())) return null;
             return new URLConnection(url) {
 
                 @Override
@@ -203,7 +217,7 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
 
                 @Override
                 public InputStream getInputStream() throws IOException {
-                    return new ByteArrayInputStream(SingletonURLStreamHandler.this.byteArray);
+                    return new ByteArrayInputStream(ModMixinConfigPlugin.this.additionalUrls.get(this.getURL().getPath()));
                 }
 
                 @Override
